6dd1b215e4165115b7057c883d0eff5726e9e1b8
Dmitriy <tdmitriy@rocketfirm.com>
2019-11-24 20:28:20 +0600

    feat(app.js, random-planet.js, swapi-service.js): show random planet

--- src/components/random-planet/random-planet.js
+++ src/components/random-planet/random-planet.js
@@ -1,29 +1,58 @@
 import React, { Component } from "react";
-
 import "./random-planet.css";
+import SwapiService from "../../services/swapi-service";
 
 export default class RandomPlanet extends Component {
+  swapi = new SwapiService();
+
+  constructor() {
+    super();
+    this.updatePlanet();
+  }
+
+  state = {
+    planet: {}
+  };
+
+  updatePlanet() {
+    const id = Math.floor(Math.random() * 8) + 2;
+    this.swapi.getPlanet(id).then(planet => {
+      this.setState({ planet });
+    });
+  }
+
   render() {
+    const {
+      planet: { id, name, population, gravity, climate, terrain, diameter }
+    } = this.state;
     return (
       <div className="random-planet jumbotron rounded">
         <img
           className="planet-image"
-          src="https://starwars-visualguide.com/assets/img/planets/5.jpg"
+          src={`https://starwars-visualguide.com/assets/img/planets/${id}.jpg`}
         />
         <div>
-          <h4>Planet Name</h4>
+          <h4>{name}</h4>
           <ul className="list-group list-group-flush">
             <li className="list-group-item">
               <span className="term">Population</span>
-              <span>123124</span>
+              <span>{population}</span>
             </li>
             <li className="list-group-item">
-              <span className="term">Rotation Period</span>
-              <span>43</span>
+              <span className="term">Climate</span>
+              <span>{climate}</span>
+            </li>
+            <li className="list-group-item">
+              <span className="term">Terrain</span>
+              <span>{terrain}</span>
             </li>
             <li className="list-group-item">
               <span className="term">Diameter</span>
-              <span>100</span>
+              <span>{diameter}</span>
+            </li>
+            <li className="list-group-item">
+              <span className="term">Gravity</span>
+              <span>{gravity}</span>
             </li>
           </ul>
         </div>
--- src/services/swapi-service.js
+++ src/services/swapi-service.js
@@ -17,25 +17,65 @@ export default class SwapiService {
     return res.results;
   }
 
-  getPerson(id) {
-    return this.getResource(`/people/${id}/`);
+  async getPerson(id) {
+    const person = await this.getResource(`/people/${id}/`);
+    return this._transformPerson(person);
   }
 
   async getAllPlanets() {
     const res = await this.getResource("/planets/");
-    return res.results;
+    return res.results.map(this._tranformPlanet);
   }
 
-  getPlanet(id) {
-    return this.getResource(`/planet/${id}/`);
+  async getPlanet(id) {
+    const planet = await this.getResource(`/planets/${id}/`);
+    return this._tranformPlanet(planet);
   }
 
   async getAllStarships() {
     const res = await this.getResource("/starships/");
-    return res.results;
+    return res.results.map(this._transformStarship);
   }
 
   getStarship(id) {
     return this.getResource(`/starships/${id}/`);
+  }
+
+  _extractId(item) {
+    const regIdExp = /\/([0-9]*)\/$/;
+    return item.url.match(regIdExp)[1];
+  }
+  _tranformPlanet(planet) {
+    return {
+      id: this._extractId(planet),
+      name: planet.name,
+      population: planet.population,
+      climate: planet.climate,
+      terrain: planet.terrain,
+      diameter: planet.diameter,
+      gravity: planet.gravity
+    };
+  }
+  _transformStarship(starship) {
+    return {
+      id: this._extractId(starship),
+      name: starship.name,
+      model: starship.model,
+      manufacturer: starship.manufacturer,
+      costInCredits: starship.costInCredits,
+      length: starship.length,
+      crew: starship.crew,
+      passengers: starship.passengers,
+      cargoCapacity: starship.cargoCapacity
+    };
+  }
+  _transformPerson(person) {
+    return {
+      id: this._extractId(person),
+      name: person.name,
+      gender: person.gender,
+      birthYear: person.birthYear,
+      eyeColor: person.eyeColor
+    };
   }
 }
